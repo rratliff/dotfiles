7f84756225f6c5f1b48a6faabdced1fba3f5f812
Bobby Ratliff <ratliff.bobby@gmail.com>
2013-08-28 16:40:47 -0500

    findall.sh: Add documentation

--- bin/findall.sh
+++ bin/findall.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+# Simple "find in files" tool
+# Parameter: the search team that is passed to grep
+# output: a list of filenames that contain 1 or more occurrences of the search
+
+# The command uses the working directory as the starting point
+# Note: currently all files are passed to grep. This is a bit inefficient,
+# as usually we are only interested in source files (e.g. *.cpp) or other
+# text data.
 
 find . -type f -print0| xargs -0 grep -c $1 | grep -v ":0$"
+
