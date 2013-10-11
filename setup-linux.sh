7aa3a9733facd25c18c196138e1b1813a71c0909
Bobby Ratliff <ratliff.bobby@gmail.com>
2013-10-10 20:05:34 -0500

    Use symlinks instead of copying over
    * Use symlinks (ln -s) to place dot files in the home directory
    * Loop through files that have .symlink instead of explicitly listing them
    * Backup files in the home before linking

--- setup-linux.sh
+++ setup-linux.sh
@@ -2,28 +2,50 @@
 # Every VM I setup I seem to do similar steps, so here is a script
 # that performs them
 
-cp .vimrc ../
-cp .bashrc ../
-cp .gitconfig ../
+DOTFILES_ROOT="`pwd`"
 
-sudo apt-get install synaptic
-sudo apt-get install ssh
-sudo apt-get install vim-nox
-sudo apt-get install gitk git-gui
-sudo apt-get install build-essential
-sudo apt-get install dkms linux-headers-generic #needed by guest additions
+install_dotfiles () {
+    echo 'installing dotfiles'
 
-# TODO Probably could remove LibreOffice packages
-# (these take up a lot of space but are not needed in a developer environment)
-# TODO Set the bash prompt to something more colorful
+    for source in `find $DOTFILES_ROOT -name \*.symlink`
+    do
+        dest="$HOME/.`basename \"${source%.*}\"`"
 
-# Setup node from the legacy ppa
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:chris-lea/node.js-legacy
-sudo apt-get update
-sudo apt-get install nodejs npm
+        if [ -f $dest ] || [ -d $dest ]
+        then
+            mv $dest $dest\.backup
+            echo "moved $dest to $dest.backup"
+        fi
 
-# Setup mysql
-sudo apt-get install mysql-client mysql-server
+        ln -s $source $dest
+    done
+}
 
-# 
+install_packages () {
+    sudo apt-get install synaptic
+    sudo apt-get install ssh
+    sudo apt-get install vim-nox
+    sudo apt-get install gitk git-gui
+    sudo apt-get install build-essential
+    sudo apt-get install dkms linux-headers-generic #needed by guest additions
+
+    # remove LibreOffice packages
+    # (these take up a lot of space and are not needed in a developer
+    # environment)
+    sudo apt-get remove --purge libreoffice*
+    sudo apt-get clean
+    sudo apt-get autoremove
+
+    # TODO Set the bash prompt to something more colorful
+
+    # Setup node from the legacy ppa
+    sudo apt-get install python-software-properties
+    sudo add-apt-repository ppa:chris-lea/node.js-legacy
+    sudo apt-get update
+    sudo apt-get install nodejs npm
+
+    # Setup mysql
+    sudo apt-get install mysql-client mysql-server
+}
+
+install_dotfiles
